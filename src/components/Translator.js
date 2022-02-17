74c750a8eea0266642ad3d1213e22147b31763ec
lissalad <elissalaymon@gmail.com>
2022-02-16 20:53:00 -0800

    mirrors  message

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -1,26 +1,35 @@
 import Down from "../icons/down";
+import { useState, useRef } from "react";
 
 function Translator() {
   const chunk = "flex flex-col items-start mb-5 mt-0";
-  const box = "border-2 border-slate-200 h-[150px] w-72";
+  const box = "border-2 border-slate-200 h-[150px] w-72 text-left p-2";
   const label = "ml-4 mb-1";
+
+  const [translation, setTranslation] = useState("");
+
+  const messageRef = useRef();
+
+  function onClick() {
+    setTranslation(messageRef.current.value)
+  }
 
   return (
     <div className="flex flex-col items-center w-full justify-evenly my-5">
       <div className={chunk}>
         <label for="incel" className={label}>Incel</label>
-        <textarea className={box} id="incel" name="incel" required />
+        <textarea ref={ messageRef } className={box} id="incel" name="incel" required />
       </div>
 
       <Down />
 
       <div className={chunk}>
         <p className={label}>Normie</p>
-        <p className={box} />
+        <p className={box}>{translation}</p>
       </div>
 
       <div className="">
-        <button className="bg-slate-200 p-2 rounded m-7" type="submit">Translate</button>
+        <button className="bg-slate-200 hover:bg-slate-300 hover:shadow p-2 rounded m-7" onClick={onClick}>Translate</button>
       </div>
 
     </div>
