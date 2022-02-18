c3f89e89f90cdabe21294b6e007d1c4350afd83b
lissalad <elissalaymon@gmail.com>
2022-02-18 11:17:19 -0800

    plural and other forms doable

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -14,38 +14,48 @@ function Translator() {
   const messageRef = useRef();
 
   function onClick() {
-    let words = messageRef.current.value.split(" ");
-    console.log(terms[0].word)
+    let input = messageRef.current.value.split(" ");
 
-    for(let word=0; word < words.length; word += 1) {
-      for(let term=0; term < terms.length; term += 1) {
-        if(words[word] === terms[term].word) {
-          words[word] = terms[term].alt;
+    // loop through words in input message
+    for (let word = 0; word < input.length; word += 1) {
+      //
+      const lower = input[word].toLowerCase();
+      // loop through term roots
+      for (let term = 0; term < terms.length; term += 1) {
+        //
+        // checks if root is in the word
+        if (lower.includes(terms[term].root)) {
+          for (let form = 0; form < terms[term].word.length; form += 1) {
+            if (lower === terms[term].word[form]) {
+              input[word] = terms[term].alt[form];
+            }
+          }
         }
       }
     }
-
-
     let output = "";
-    for(let w = 0; w < words.length - 1; w += 1) {
-      output += words[w];
+    for (let w = 0; w < input.length - 1; w += 1) {
+      output += input[w];
       output += " ";
     }
-    output += words[words.length-1];
+    output += input[input.length - 1];
 
     setTranslation(output);
   }
 
-
-
-
-
-
   return (
     <div className="flex flex-col items-center w-full justify-evenly my-5">
       <div className={chunk}>
-        <label for="incel" className={label}>Incel</label>
-        <textarea ref={ messageRef } className={box} id="incel" name="incel" required />
+        <label htmlFor="incel" className={label}>
+          Incel
+        </label>
+        <textarea
+          ref={messageRef}
+          className={box}
+          id="incel"
+          name="incel"
+          required
+        />
       </div>
 
       <Down />
@@ -56,9 +66,13 @@ function Translator() {
       </div>
 
       <div className="">
-        <button className="bg-slate-200 hover:bg-slate-300 hover:shadow p-2 rounded m-7" onClick={onClick}>Translate</button>
+        <button
+          className="bg-slate-200 hover:bg-slate-300 hover:shadow p-2 rounded m-7"
+          onClick={onClick}
+        >
+          Translate
+        </button>
       </div>
-
     </div>
   );
 }
